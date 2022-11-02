a3dd9b4ef494edf957cb291018816489673194e6
MattiaLongo03 <mattilongo2003@gmail.com>
2022-11-02 16:41:18 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const eleGrid = document.querySelector('.grid');
 
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i < 101; i++) {
 	const eleCell = document.createElement('div');
 	eleCell.classList.add('cell');
 	eleGrid.append(eleCell);
+    eleCell.append(i);
 
 	eleCell.addEventListener('click', function () {
 		this.classList.toggle('active')
